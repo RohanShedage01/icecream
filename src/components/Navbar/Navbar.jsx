98e2593e1634926f4de92db7b6be6e6b39ca59d9
RohanShedage01 <shedagerohan21@gmail.com>
2026-03-18 14:17:49 +0530

    navbar setup complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import "./Navbar.css";
 import { Link } from "react-router";
-
+import { House , FileUser , HeartHandshake , Phone  } from "lucide-react";
 function Navbar() {
   return (
-    <div>
-      <Link to="/">Home</Link>&nbsp;|&nbsp;
-      <Link to="/about">About</Link>&nbsp;|&nbsp;
-      <Link to="/services">Our Services</Link>&nbsp;|&nbsp;
-      <Link to="/contact">Contact</Link>&nbsp;|&nbsp; 
+    <div className="navbar">
+      <Link to="/" className="nav-items"><House className="nav-icon"/>Home</Link>
+      <Link to="/about" className="nav-items"><FileUser className="nav-icon"/>About</Link>
+      <Link to="/services" className="nav-items"><HeartHandshake className="nav-icon"/>Our Services</Link>
+      <Link to="/contact" className="nav-items"><Phone className="nav-icon"/>Contact</Link>
     </div>
   )
 }
